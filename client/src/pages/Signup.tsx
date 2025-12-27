c6ef9a03124d4a2c87282733ed96cfc62debdc4d
RaviKumawat21 <ravikumawat8860@gmail.com>
2025-12-27 15:21:02 +0530

    nodemon

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -5,7 +5,7 @@ import { FiLock, FiMail, FiUser } from 'react-icons/fi';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 
 export default function Signup() {
   const navigate = useNavigate();
